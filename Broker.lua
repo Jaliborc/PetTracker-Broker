8d8e39fee0fedd5c82e524c4b4489442a0bf1eed
João Cardoso <jaliddons@gmail.com>
2024-08-29 23:16:15 +0100

    11.0

--- Broker.lua
+++ Broker.lua
@@ -17,10 +17,33 @@ This file is part of PetTracker Broker.
 
 local Broker = PetTracker:NewModule('Broker', LibStub('LibDataBroker-1.1'):NewDataObject('PetTracker', {
 	tocname = 'PetTracker',
+	icon = 'Interface/Addons/PetTracker/art/logo',
 	type = 'data source',
-	icon = 'Interface/Icons/Tracking_WildPet',
 	label = PETS,
 }))
+
+local LOCALE = GetLocale()
+local NONE_HERE = 'No known species in this zone.'
+
+if LOCALE == 'deDE' then
+    NONE_HERE = 'Keine bekannte Spezies in dieser Zone.'
+elseif LOCALE == 'frFR' then
+    NONE_HERE = 'Aucune espèce connue dans cette zone.'
+elseif LOCALE == 'esES' or LOCALE == 'esMX' then
+    NONE_HERE = 'No hay especies conocidas en esta zona.'
+elseif LOCALE == 'itIT' then
+    NONE_HERE = 'Nessuna specie conosciuta in questa zona.'
+elseif LOCALE == 'ptBR' or LOCALE == 'ptPT' then
+    NONE_HERE = 'Nenhuma espécie conhecida nesta zona.'
+elseif LOCALE == 'ruRU' then
+    NONE_HERE = 'В этой зоне нет известных видов.'
+elseif LOCALE == 'koKR' then
+    NONE_HERE = '이 지역에는 알려진 종이 없습니다.'
+elseif LOCALE == 'zhCN' then
+    NONE_HERE = '该区域没有已知物种。'
+elseif LOCALE == 'zhTW' then
+    NONE_HERE = '此區域中無已知物種。'
+end
 
 
 --[[ Startup ]]--
@@ -31,12 +54,12 @@ function Broker:OnEnable()
 
 	self.tip = CreateFrame('GameTooltip', 'PetTrackerBrokerTip', UIParent, 'GameTooltipTemplate')
 	self.tracker = PetTracker.Tracker(self.tip)
-	self.tracker.MaxEntries = 20
 	self.tracker:SetPoint('TOPLEFT', 15, -30)
-	self.tracker.Anchor:SetPoint('TOPLEFT')
-	self.tracker.AddSpecies = function(tracker)
-		self.tracker:GetClass().AddSpecies(tracker)
-		self:Resize()
+	self.tracker.Update = function(tracker)
+		if self.tip:IsVisible() then
+			self.tracker:GetClass().Update(tracker)
+			self:Resize()
+		end
 	end
 end
 
@@ -55,7 +78,11 @@ function Broker:Update()
 end
 
 function Broker:Resize()
-	self.tip:SetSize(265, self.tracker:GetHeight())
+	if self.tracker:IsShown() then
+		self.tip:SetSize(265, self.tracker:GetHeight())
+	else
+		self.tip:SetText(NONE_HERE)
+	end
 end
 
 
@@ -63,7 +90,7 @@ end
 
 function Broker:OnEnter()
 	Broker.tip:ClearAllPoints()
-	Broker.tip:SetOwner(self, self:GetBottom() < UIParent:GetHeight() / 2  and 'ANCHOR_TOP' or 'ANCHOR_BOTTOM')
+	Broker.tip:SetOwner(self, self:GetBottom() < UIParent:GetHeight() / 2 and 'ANCHOR_TOP' or 'ANCHOR_BOTTOM')
 	Broker.tip:SetText(PETS)
 	Broker.tip:Show()
 	Broker:Resize()
@@ -75,9 +102,9 @@ end
 
 function Broker:OnClick(button)
 	if button == 'RightButton' then
-		Broker:OnLeave()
-		Broker.tracker.ToggleDropdown(self)
+		PetTracker.ToggleOption('zoneTracker')
 	else
-		Broker.tracker:Toggle()
+		Broker:OnLeave()
+		Broker.tracker.Menu(self)
 	end
 end
--- Changelog.md
+++ Changelog.md
@@ -1,45 +1,49 @@
-##### 9.0.0
+## 11.0
+* Updated for The War Within.
+* Now uses the official PetTracker art.
+
+## 9.0
 * Updated for Shadowlands.
 * Fixed bug with specie list display.
 
-##### 8.0.1
+### 8.0.1
 * Minor update.
 
-#### 8.0
+## 8.0
 * Updated for Battle for Azeroth
 
-#### 7.1
+## 7.1
 * Updated for Return to Kharazan
 
-#### 7.0
+## 7.0
 * Updated for Legion
 
-#### 6.2
+## 6.2
 * Updated for Fury of Hellfire
 
-#### 6.1
+## 6.1
 * Updated for patch 6.1
 
-##### 6.0.1
+### 6.0.1
 * Bugfixes! Everyone loves bugfixes!
 
-#### 6.0
+## 6.0
 * Updated for The Iron Tide.
 
-##### 5.4.1
+### 5.4.1
 * The mouseover tooltip now should work with almost any addon.
 
-#### 5.4
+## 5.4
 * Updated for Siege of Ogrimmar.
 
-#### 5.3
+## 5.3
 * Updated for patch Escalation.
 
-##### 5.2.1
+### 5.2.1
 * Updated for latest PetTracker version
 
-#### 5.2
+## 5.2
 * Updated for patch The Thunder King
 
-#### 5.1
+## 5.1
 * Initial release
